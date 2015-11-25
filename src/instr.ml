a19c98bbb15bc28bdfdf031262bc3a156f173d29
Samuel Sleight <samuel.sleight@gmail.com>
2015-11-25 01:26:04 +0000

    Anonymous functions

--- src/instr.ml
+++ src/instr.ml
@@ -1,6 +1,13 @@
 (* Compiler Construction - instr.ml *)
 (* Samuel Sleight *)
 
+let rec idx item = function
+    | [] -> None 
+    | hd::tl -> if item = hd then (Some 0) else begin match idx item tl with
+        | Some n -> Some (n + 1)
+        | None -> None
+    end
+
 (* Value types *)
 type binop_t =
     | Add
@@ -10,6 +17,7 @@ type binop_t =
 
 type fn_t = 
     | BinOp of binop_t
+    | Named of string
 
 type io_t =
     | AsChar
@@ -23,6 +31,7 @@ type value_source =
     | Const of value_t
     | BinOp of binop_t * value_source * value_source
     | Read of io_t
+    | Arg of int
     | Stored of int
 
 (* Instruction types *)
@@ -36,7 +45,7 @@ type 'a env = {
     env: 'a list;
     parent: ('a env) option;
     args: string list;
-    arg_values: (instruction_t list) option;
+    arg_values: (value_source list) option;
     name: string;
 }
 
@@ -53,6 +62,18 @@ let make_env ?(parent=None) ?(name="") ?(args=[]) env = {
     arg_values = None;
     name = name;
 }
+
+let rec make_env_name env = match env.parent with
+    | Some parent -> (make_env_name parent) ^ "_" ^ env.name
+    | None -> env.name
+
+let env_lookup env name values = match idx name env.args with
+    | Some n -> begin match env.arg_values with
+        | Some l -> (List.nth l n) :: values
+        | None -> (Arg n) :: values
+    end
+
+    | None -> values
 
 (* Result type *)
 type result_fn = {
@@ -72,11 +93,13 @@ let string_of_binop = function
 let string_of_value = function
     | Int i -> string_of_int i
     | Fn (BinOp op) -> string_of_binop op
+    | Fn (Named n) -> n
 
 let rec string_of_value_source = function
     | Const v -> string_of_value v
     | BinOp (op, x, y) -> string_of_binop op ^ "[" ^ string_of_value_source x ^ ":" ^ string_of_value_source y ^ "]"
     | Read AsChar -> "Read[Char]"
+    | Arg n -> "Arg[" ^ string_of_int n ^ "]"
     | Stored n -> "Stored[" ^ string_of_int n ^ "]"
 
 let string_of_instr = function
@@ -101,6 +124,16 @@ let string_of_fns fns = begin
 end
 
 (* Impl *)
+let rec pop_args n values = if n = 0 then 
+    Some ([], values)
+else match values with
+    | [] -> None
+
+    | v :: rest -> begin match pop_args (n - 1) rest with
+        | Some (args, more) -> Some (v :: args, more)
+        | None -> None
+    end
+
 let push_string values str = begin
     let ls = ref [] in
     String.iter (fun c -> ls := Const (Int (int_of_char c)) :: !ls) str;
@@ -116,8 +149,11 @@ let apply_binop values op x y = match (op, x, y) with
     | _ -> BinOp (op, x, y) :: values
 
 let generate_instrs opt_flags code =
-    let errors = ref [] in
+    let next_id = let prev = ref 0 in (fun () -> prev := !prev + 1; !prev) in
+
+    let fn_envs = ref Fns.empty in
     let result = ref Fns.empty in
+    let errors = ref [] in
 
     let rec generate_function instrs values env stored = function
         | [] -> List.rev instrs
@@ -126,6 +162,20 @@ let generate_instrs opt_flags code =
             | Syntax.Value (Syntax.Int i) -> generate_function instrs (Const (Int i) :: values) env stored code
             | Syntax.Value (Syntax.Char c) -> generate_function instrs (Const (Int (int_of_char c)) :: values) env stored code
             | Syntax.Value (Syntax.String s) -> generate_function instrs (push_string values s) env stored code
+
+            (* Anonymous Functions *)
+            | Syntax.Value (Syntax.Function (fn_args, fn_prog)) -> begin
+                let fn_id = next_id () in
+                let fn_name = (make_env_name env) ^ "_anon_" ^ (string_of_int fn_id) in
+                let fn_env = make_env ~parent:(Some env) ~name:("anon_" ^ string_of_int fn_id) ~args:fn_args fn_prog.Syntax.env in
+                fn_envs := Fns.add fn_name (fn_env, fn_prog.Syntax.code) !fn_envs;
+                let fn = generate_function [] [] fn_env 0 fn_prog.Syntax.code in
+                result := Fns.add fn_name { code=fn; args=(List.length fn_args); } !result;
+                generate_function instrs (Const (Fn (Named fn_name)) :: values) env stored code
+            end
+
+            (* Named Value *)
+            | Syntax.Value (Syntax.Ident n) -> generate_function instrs (env_lookup env n values) env stored code
 
             (* Binary Operators *)
             | Syntax.Op Syntax.Plus -> generate_function instrs (Const (Fn (BinOp Add)) :: values) env stored code
@@ -141,6 +191,20 @@ let generate_instrs opt_flags code =
                         errors := Errors.not_enough_args exp_block.Syntax.location :: !errors;
                         generate_function instrs values env stored code
                     end
+                end
+
+                | Const (Fn (Named n)) :: rest -> begin
+                    let (fn_env, fn_code) = Fns.find n !fn_envs in
+                    match pop_args (List.length fn_env.args) rest with
+                        Some (args, rest) -> generate_function 
+                            (List.rev_map (fun arg -> Store arg) args @ instrs)
+                            rest
+                            { fn_env with 
+                                arg_values=(Some (List.mapi (fun n _ -> Stored (stored + n)) fn_env.args));
+                                parent=(Some env);
+                            }
+                            (stored + (List.length fn_env.args))
+                            (fn_code @ { Syntax.location=Lexing.dummy_pos; Syntax.data=Syntax.PopEnv; } :: code)
                 end
             end
 
@@ -160,12 +224,18 @@ let generate_instrs opt_flags code =
                 | Stored n :: _ -> generate_function (WriteStored (AsInt, n) :: instrs) values env stored code
                 | value :: rest -> generate_function (WriteStored (AsInt, stored) :: Store value :: instrs) (Stored stored :: rest) env (stored + 1) code
             end
+
+            (* Pop Environment *)
+            | Syntax.PopEnv -> begin match env.parent with
+                | Some parent -> generate_function instrs values parent stored code
+                | None -> generate_function instrs values env stored code
+            end
         end
     in
 
     let fn = generate_function [] [] (make_env code.Syntax.env) 0 code.Syntax.code in
 
     if !errors = [] then
-        Errors.Ok (Fns.add "" { code=fn; args=0 } !result)
+        Errors.Ok (Fns.add "" { code=fn; args=0 } Fns.empty)
     else
         Errors.Err !errors
