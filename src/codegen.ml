260e5bfd1bb0c0bd63a6dd290d5cbfb58c9e230c
Samuel Sleight <samuel.sleight@gmail.com>
2015-11-25 18:08:11 +0000

    Function returning

--- src/codegen.ml
+++ src/codegen.ml
@@ -4,33 +4,49 @@
 open Llvm
 
 module Values = Map.Make(struct type t = int let compare = compare end)
+module Fns = Map.Make(String)
 
 let generate_code opt_flags size_flags fns = 
     let ctx = global_context () in
     let mdl = create_module ctx "sfl_mdl" in
     let bld = builder ctx in
 
+    let used_fns = ref Fns.empty in
+
     (* Typedefs *)
     let void_t = void_type ctx in
     let int_t = integer_type ctx 64 in
+    let ptr_t = pointer_type int_t in
 
     let main_t = function_type void_t [||] in
+    let func_t returns = function_type void_t (Array.make (if returns = 0 then 1 else 2) ptr_t) in
 
-    let get_fn fn_t name = declare_function name fn_t mdl in
+    let get_fn fn_t name = try
+        Fns.find name !used_fns
+    with
+        Not_found -> begin
+            let fn = declare_function name fn_t mdl in
+            used_fns := Fns.add name fn !used_fns;
+            fn
+        end
+    in
     
     let get_fn_putchar () = get_fn (function_type void_t [|int_t|]) "putchar" in
     let get_fn_getchar () = get_fn (function_type int_t [||]) "getchar" in
 
     (* Codegen *)
     let generate_function name code = 
-        let code_fn = get_fn main_t name in
+        let fn_t = if name = "main" then main_t else (func_t code.Instr.returns) in
+        let code_fn = get_fn fn_t name in
         let block = append_block ctx "entry" code_fn in
+
+        set_function_call_conv (if name = "main" then CallConv.c else CallConv.fast) code_fn;
         position_at_end block bld;
 
         let next_value = let next = ref 0 in (fun () -> next := !next + 1; !next - 1) in
         let stored_values = ref Values.empty in
 
-        let rec generate_value = function
+        let rec generate_value  = function
             | Instr.Const (Instr.Int i) -> const_int int_t i
             | Instr.Stored n -> Values.find n !stored_values
             | Instr.Read Instr.AsChar -> build_call (get_fn_getchar ()) [||] "" bld
@@ -43,6 +59,12 @@ let generate_code opt_flags size_flags fns =
                     | Instr.Sub -> build_sub x_v y_v "" bld
                     | Instr.Mul -> build_mul x_v y_v "" bld
                     | Instr.Div -> build_sdiv x_v y_v "" bld
+            end
+
+            | Instr.Arg n -> begin
+                let args = param code_fn 0 in
+                let ep = build_gep args [|const_int int_t n|] "" bld in
+                build_load ep "" bld
             end
         in
 
@@ -61,12 +83,49 @@ let generate_code opt_flags size_flags fns =
                 let value = generate_value v in
                 stored_values := Values.add (next_value ()) value !stored_values
             end
+
+            | Instr.Apply (n, args, ret) -> begin
+                let mem = build_array_alloca int_t (const_int int_t (ret + (List.length args))) "" bld in
+
+                List.iteri (fun i arg ->
+                    let ep = build_gep mem [|const_int int_t i|] "" bld in
+                    ignore (build_store (generate_value arg) ep bld)
+                ) args;
+
+                if ret = 0 then
+                    ignore (build_call (get_fn (func_t ret) n) [|mem|] "" bld)
+                else
+                    let retp = build_gep mem [|const_int int_t (List.length args)|] "" bld in
+                    let rec make_ret_store retn =
+                        if retn = 0 then
+                            ()
+                        else begin
+                            let ep = build_gep retp [|const_int int_t (retn - 1)|] "" bld in
+                            stored_values := Values.add (next_value ()) (build_load ep "" bld) !stored_values;
+                            make_ret_store (retn - 1)
+                        end
+                    in
+
+                    build_call (get_fn (func_t ret) n) [|mem; retp|] "" bld;
+                    make_ret_store ret
+            end
+            
+            | Instr.Return ret -> begin
+                if not (name = "main") then begin
+                    let rets = param code_fn 1 in
+                    List.iteri (fun i v ->
+                        let ep = build_gep rets [|const_int int_t i|] "" bld in
+                        ignore (build_store (generate_value v) ep bld)
+                    ) ret
+                end else
+                    ()
+            end
         in
 
-        List.iter generate_instr code;
+        List.iter generate_instr code.Instr.code;
         ignore (build_ret_void bld)
     in
 
 
-    Instr.Fns.iter (fun name instrs -> generate_function (if name = "" then "main" else name) instrs.Instr.code) fns;
+    Instr.Fns.iter (fun name instrs -> generate_function (if name = "" then "main" else name) instrs) fns;
     Errors.Ok (string_of_llmodule mdl)
--- src/instr.ml
+++ src/instr.ml
@@ -14,9 +14,14 @@ type binop_t =
     | Sub
     | Mul
     | Div
+    | Eq
+
+type triop_t =
+    | Ite
 
 type fn_t = 
     | BinOp of binop_t
+    | TriOp of triop_t
     | Named of string
 
 type io_t =
@@ -30,6 +35,7 @@ type value_t =
 type value_source =
     | Const of value_t
     | BinOp of binop_t * value_source * value_source
+    | TriOp of triop_t * value_source * value_source * value_source
     | Read of io_t
     | Arg of int
     | Stored of int
@@ -39,6 +45,8 @@ type instruction_t =
     | WriteConst of io_t * value_t
     | WriteStored of io_t * int
     | Store of value_source
+    | Apply of string * (value_source list) * int
+    | Return of value_source list
 
 (* Env type *)
 type 'a env = {
@@ -46,10 +54,10 @@ type 'a env = {
     parent: ('a env) option;
     args: string list;
     arg_values: (value_source list) option;
-    name: string;
+    name: string option;
 }
 
-let make_env ?(parent=None) ?(name="") ?(args=[]) env = {
+let make_env ?(parent=None) ?(name=Some("")) ?(args=[]) env = {
     env = Syntax.Env.fold (fun name value acc -> match value.Syntax.data with
         | Syntax.Int i -> `Int (name, i) :: acc
         | Syntax.Char c -> `Int (name, int_of_char c) :: acc
@@ -64,13 +72,21 @@ let make_env ?(parent=None) ?(name="") ?(args=[]) env = {
 }
 
 let rec make_env_name env = match env.parent with
-    | Some parent -> (make_env_name parent) ^ "_" ^ env.name
-    | None -> env.name
+    | Some parent -> begin match env.name with
+        | Some name -> (make_env_name parent) ^ "_" ^ name
+        | None -> make_env_name parent
+    end
+
+    | None -> begin match env.name with
+        | Some name -> name
+        | None -> ""
+    end
 
 (* Result type *)
 type result_fn = {
     code: instruction_t list;
-    args: int
+    args: int;
+    returns: int;
 }
 
 module Fns = Map.Make(String)
@@ -81,18 +97,30 @@ let string_of_binop = function
     | Sub -> "Sub"
     | Mul -> "Mul"
     | Div -> "Div"
+    | Eq -> "Eq"
+
+let string_of_triop = function
+    | Ite -> "Ite"
 
 let string_of_value = function
     | Int i -> string_of_int i
     | Fn (BinOp op) -> string_of_binop op
+    | Fn (TriOp op) -> string_of_triop op
     | Fn (Named n) -> n
 
 let rec string_of_value_source = function
     | Const v -> string_of_value v
     | BinOp (op, x, y) -> string_of_binop op ^ "[" ^ string_of_value_source x ^ ":" ^ string_of_value_source y ^ "]"
+    | TriOp (op, x, y, z) -> string_of_triop op ^ "[" ^ string_of_value_source x ^ ":" ^ string_of_value_source y ^ ":" ^ string_of_value_source z ^ "]"
     | Read AsChar -> "Read[Char]"
     | Arg n -> "Arg[" ^ string_of_int n ^ "]"
     | Stored n -> "Stored[" ^ string_of_int n ^ "]"
+
+let string_of_arg_list ls = begin
+    let buf = Buffer.create 20 in
+    List.iteri (fun i v -> Buffer.add_string buf ((if i = 0 then "" else ":") ^ string_of_value_source v)) ls;
+    Buffer.contents buf;
+end
 
 let string_of_instr = function
     | WriteConst (AsChar, v) -> "[Write[Char:" ^ string_of_value v ^ "]]"
@@ -100,6 +128,8 @@ let string_of_instr = function
     | WriteConst (AsInt, v) -> "[Write[Int:" ^ string_of_value v ^ "]]"
     | WriteStored (AsInt, v) -> "[Write[Int:Stored[" ^ string_of_int v ^ "]]]"
     | Store v -> "[Store " ^ string_of_value_source v ^ "]"
+    | Apply (n, v, _) -> "[Apply " ^ n ^ "[" ^ string_of_arg_list v ^ "]]"
+    | Return v -> "[Return [" ^ string_of_arg_list v ^ "]]"
 
 let string_of_fn name fn = begin
     let buf = Buffer.create 20 in
@@ -137,8 +167,34 @@ let apply_binop values op x y = match (op, x, y) with
     | (Sub, Const (Int x), Const (Int y)) -> Const (Int (x - y)) :: values
     | (Mul, Const (Int x), Const (Int y)) -> Const (Int (x * y)) :: values
     | (Div, Const (Int x), Const (Int y)) -> Const (Int (x / y)) :: values
+    | (Eq, Const (Int x), Const (Int y)) -> Const (Int (if x = y then 1 else 0)) :: values
 
     | _ -> BinOp (op, x, y) :: values
+
+let apply_triop values op x y z = match (op, x, y, z) with
+    | (Ite, Const (Int x), y, z) -> (if x = 0 then z else y) :: values
+
+    | _ -> TriOp (op, x, y, z) :: values
+
+let rec arg_instrs instrs = function
+    | [] -> instrs
+    | Const c :: args -> arg_instrs instrs args
+    | arg :: args -> Store arg :: arg_instrs instrs args
+
+let rec arg_values stored = function
+    | [] -> (stored, [])
+
+    | Const c :: args -> let (s, ls) = arg_values stored args in
+        (s, Const c :: ls)
+
+    | arg :: args -> let (s, ls) = arg_values (stored + 1) args in
+        (s, Stored stored :: ls)
+
+let rec make_ret args stored values =
+    if args = 0 then
+        values
+    else
+        make_ret (args - 1) (stored + 1) (Stored stored :: values)
 
 let generate_instrs opt_flags code =
     let next_id = let prev = ref 0 in (fun () -> prev := !prev + 1; !prev) in
@@ -160,10 +216,10 @@ let generate_instrs opt_flags code =
                     Some (Const (Fn (Named fn_name)) :: values)
                 with
                     Not_found -> begin
-                        let fn_env = make_env ~parent:(Some env) ~name:name ~args:a p.Syntax.env in
+                        let fn_env = make_env ~parent:(Some env) ~name:(Some name) ~args:a p.Syntax.env in
                         fn_envs := Fns.add fn_name (fn_env, p.Syntax.code) !fn_envs;
-                        let fn = generate_function [] [] fn_env 0 p.Syntax.code in
-                        result := Fns.add fn_name { code=fn; args=(List.length a); } !result;
+                        let (ret, fn) = generate_function [] [] fn_env 0 p.Syntax.code in
+                        result := Fns.add fn_name { code=fn; args=(List.length a); returns=ret; } !result;
                         Some (Const (Fn (Named fn_name)) :: values)
                     end
             end else 
@@ -181,12 +237,16 @@ let generate_instrs opt_flags code =
                     | None -> (Arg n) :: values
                 end
 
-                (* TODO *)
-                | None -> values
+                (* Lookup in parent *)
+                | None -> begin match env.parent with
+                    | Some parent -> env_lookup parent name values
+                    | None -> values (* TODO actual error *)
+                end
             end
 
     and generate_function instrs values env stored = function
-        | [] -> List.rev instrs
+        | [] -> (List.length values, List.rev (Return values :: instrs))
+
         | exp_block :: code -> begin match exp_block.Syntax.data with
             (* Basic Values *)
             | Syntax.Value (Syntax.Int i) -> generate_function instrs (Const (Int i) :: values) env stored code
@@ -197,10 +257,10 @@ let generate_instrs opt_flags code =
             | Syntax.Value (Syntax.Function (fn_args, fn_prog)) -> begin
                 let fn_id = next_id () in
                 let fn_name = (make_env_name env) ^ "_anon_" ^ (string_of_int fn_id) in
-                let fn_env = make_env ~parent:(Some env) ~name:("anon_" ^ string_of_int fn_id) ~args:fn_args fn_prog.Syntax.env in
+                let fn_env = make_env ~parent:(Some env) ~name:(Some ("anon_" ^ string_of_int fn_id)) ~args:fn_args fn_prog.Syntax.env in
                 fn_envs := Fns.add fn_name (fn_env, fn_prog.Syntax.code) !fn_envs;
-                let fn = generate_function [] [] fn_env 0 fn_prog.Syntax.code in
-                result := Fns.add fn_name { code=fn; args=(List.length fn_args); } !result;
+                let (ret, fn) = generate_function [] [] fn_env 0 fn_prog.Syntax.code in
+                result := Fns.add fn_name { code=fn; args=(List.length fn_args); returns=ret; } !result;
                 generate_function instrs (Const (Fn (Named fn_name)) :: values) env stored code
             end
 
@@ -212,6 +272,8 @@ let generate_instrs opt_flags code =
             | Syntax.Op Syntax.Minus -> generate_function instrs (Const (Fn (BinOp Sub)) :: values) env stored code
             | Syntax.Op Syntax.Times -> generate_function instrs (Const (Fn (BinOp Mul)) :: values) env stored code
             | Syntax.Op Syntax.Divide -> generate_function instrs (Const (Fn (BinOp Div)) :: values) env stored code
+            | Syntax.Op Syntax.Eq -> generate_function instrs (Const (Fn (BinOp Eq)) :: values) env stored code
+            | Syntax.Op Syntax.IfThenElse -> generate_function instrs (Const (Fn (TriOp Ite)) :: values) env stored code
 
             (* Application *)
             | Syntax.Apply Syntax.Full -> begin match values with
@@ -223,19 +285,57 @@ let generate_instrs opt_flags code =
                     end
                 end
 
-                | Const (Fn (Named n)) :: rest -> begin
+                | Const (Fn (TriOp op)) :: rest -> begin match rest with
+                    | x :: y :: z :: rest -> generate_function instrs (apply_triop rest op x y z) env stored code
+                    | _ -> begin
+                        errors := Errors.not_enough_args exp_block.Syntax.location :: !errors;
+                        generate_function instrs values env stored code
+                    end
+                end
+
+                | Const (Fn (Named n)) :: rest -> if opt_flags.Flag.fe then begin
+                    (* Attempt inlining of function *)
                     let (fn_env, fn_code) = Fns.find n !fn_envs in
                     match pop_args (List.length fn_env.args) rest with
-                        Some (args, rest) -> generate_function 
-                            (List.rev_map (fun arg -> Store arg) args @ instrs)
-                            rest
-                            { fn_env with 
-                                arg_values=(Some (List.mapi (fun n _ -> Stored (stored + n)) fn_env.args));
-                                parent=(Some env);
-                            }
-                            (stored + (List.length fn_env.args))
-                            (fn_code @ { Syntax.location=Lexing.dummy_pos; Syntax.data=Syntax.PopEnv; } :: code)
+                        | Some (args, rest) -> 
+                            let (stored, values) = arg_values stored args in
+                            generate_function 
+                                (arg_instrs instrs args)
+                                rest
+                                { fn_env with 
+                                    arg_values=Some(values);
+                                    parent=(Some env);
+                                    name=None;
+                                }
+                                stored
+                                (fn_code @ { Syntax.location=Lexing.dummy_pos; Syntax.data=Syntax.PopEnv; } :: code)
+
+                        (* Cannot inline - TODO *)
+                        | None -> begin
+                            errors := Errors.not_enough_args exp_block.Syntax.location :: !errors;
+                            generate_function instrs values env stored code
+                        end
+
+                end else begin
+                    let fn = Fns.find n !result in
+                    match pop_args fn.args rest with
+                        | Some (args, rest) -> 
+                            generate_function 
+                                (Apply (n, args, fn.returns) :: instrs) 
+                                (make_ret fn.returns stored rest) 
+                                env 
+                                (stored + fn.returns) 
+                                code
+
+                        (* Cannot apply - TODO *)
+                        | None -> begin
+                            errors := Errors.not_enough_args exp_block.Syntax.location :: !errors;
+                            generate_function instrs values env stored code
+                        end
+
                 end
+
+                | _ -> generate_function (Apply ("", [], 0) :: instrs) values env stored code
             end
 
             (* Read *)
@@ -263,9 +363,9 @@ let generate_instrs opt_flags code =
         end
     in
 
-    let fn = generate_function [] [] (make_env code.Syntax.env) 0 code.Syntax.code in
+    let (_, fn) = generate_function [] [] (make_env code.Syntax.env) 0 code.Syntax.code in
 
     if !errors = [] then
-        Errors.Ok (Fns.add "" { code=fn; args=0 } Fns.empty)
+        Errors.Ok (Fns.add "" { code=fn; args=0; returns=0; } !result)
     else
         Errors.Err !errors
